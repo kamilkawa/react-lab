83748036dddc3391a93813ec58ab825e6a19d9cc
Kamil Kawa <numeryczny_wzbudzac0m@icloud.com>
2025-12-14 11:45:59 +0100

    adding reacting part: hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E'
+    const [title, setTitle] = useState('Wall-E');
     function handleChange(e) {
-        console.log(e.target.value)
+        setTitle(e.target.value);
     }
+
+    let message = '';
+    if (title.length <5){
+        message = "Title is too short.";
+    }
+    else if (title.length>=5 & title.length<10){
+        message = "Title is super.";
+    }
+    else {
+        message = "Title is too long.";
+    }
+    
 
     return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
+          {
+            title.length > 0 && <div>{message}</div>
+          }
           <input type='text' onChange={handleChange} />
       </div>
   );
