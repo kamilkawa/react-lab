4435c69c80b5a8a55e94359d72b00517457069bc
Kamil Kawa <numeryczny_wzbudzac0m@icloud.com>
2026-01-17 12:05:40 +0100

    deploy to github pages and functionality to hide a form for adding movies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
 
-
 function App() {
-   const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
-   return (
-        <div>
-           <h1>My favourite movies to watch</h1>
-           <MoviesList movies={movies}/>
-           <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
-        </div>
-   );
+  function handleMovieSubmit(movie) {
+    setMovies(prev => [...prev, movie]); // dodaj film
+    setShowForm(false);                  // schowaj formularz
+  }
+
+  return (
+    <div>
+      <h1>My favourite movies to watch</h1>
+
+      <MoviesList movies={movies} />
+
+      <button onClick={() => setShowForm(prev => !prev)}>
+        {showForm ? "Hide adding movie form" : "Add a movie"}
+      </button>
+
+      {showForm && (
+        <MovieForm onMovieSubmit={handleMovieSubmit} />
+      )}
+    </div>
+  );
 }
+
 export default App;
