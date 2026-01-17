5bc97a84438bbd6ef848f6e51e6e398d4d5dc1f0
Kamil Kawa <numeryczny_wzbudzac0m@icloud.com>
2026-01-17 11:43:23 +0100

    movies list created as a seperate component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 import {useState} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
+
 
 function App() {
-    const [movies, setMovies] = useState([{title: "Well-E"}, {title: "2012"}]);
-    return (
-      <div className="container">
-          <h1>My favourite movies to watch</h1>
-          <h2>Titles</h2>
-          <ul>
-            {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
-          </ul>
-          {/* This is the way to add a movie string into existing movies list */}
-          <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
-      </div>
-  );
-}
+   const [movies, setMovies] = useState([]);
 
+   return (
+        <div>
+           <h1>My favourite movies to watch</h1>
+           <MoviesList movies={movies}/>
+           <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
+        </div>
+   );
+}
 export default App;
