e53fc718ae0f7743895008d36e0acb5bbe0b9ed8
Kamil Kawa <numeryczny_wzbudzac0m@icloud.com>
2026-01-17 11:00:11 +0100

    refactoring application into components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import MovieForm from "./MovieForm";
 
 function App() {
-    // const [title, setTitle] = useState('Wall-E');
-    // const movies = [
-    //     {title: "Wall-E"},
-    //     {title: "Pulp Fiction"},
-    //     {title: "Matrix"},
-    //     {title: "1670"},
-    // ];
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
-    const [movies, setMovies] = useState([]);
-
-    let message = '';
-    if (title.length <5){
-        message = "Title is too short.";
-    }
-    else if (title.length>=5 & title.length<10){
-        message = "Title is super.";
-    }
-    else {
-        message = "Title is too long.";
-    }
-    
-    function addMovie(event) {
-        event.preventDefault();
-        setMovies([...movies, {title, year}]);
-        setTitle('');
-        setYear('');
-    }
-
+    const [movies, setMovies] = useState([{title: "Well-E"}, {title: "2012"}]);
     return (
       <div className="container">
           <h1>My favourite movies to watch</h1>
@@ -39,18 +12,8 @@ function App() {
           <ul>
             {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
           </ul>
-          <form onSubmit={addMovie}>
-            <div>
-                <label>Tytuł</label>
-                    <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
-                    {title.length > 0 && <div>{message}</div>}
-                </div>
-                <div>
-                    <label>Rok nagrania</label>
-                    <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
-                </div>
-                <button>Dodaj film</button>
-            </form>
+          {/* This is the way to add a movie string into existing movies list */}
+          <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
       </div>
   );
 }
